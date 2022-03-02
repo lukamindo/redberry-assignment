46d82385c05aa361c7f03e9b1575939dbcd65cdc
lukamind <luka.mindiashvili14@gmail.com>
2022-03-02 21:28:01 +0400

    add initial page components

---
+++ src/components/Heading.js
@@ -0,0 +1,8 @@
+import "./Heading.css"
+
+
+export default function Heading (props) {
+    return (
+        <h1 className="Heading">{props.children}</h1>
+    )
+}   
---
+++ src/components/Page.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import "./Page.css";
+import Heading from "./Heading";
+
+export default function Page(props) {
+  return (
+    <div
+      className={`Page ${props.theme === "light" ? "Page-light" : "Page-dark"}`} 
+    >   
+      <Heading>{props.title}</Heading>
+
+      {props.children}
+
+    </div>
+  );
+}
+
+Page.propTypes = {
+    title: PropTypes.string,
+    theme: PropTypes.oneOf(['light', 'dark'])
+}
+
+Page.defaultProps = {
+    theme: 'dark'
+  };
